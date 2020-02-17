7c307e34d2a6cadf1d5b0c0c6c6fcd0346db2fc7
Karthikeyan <karthikeyan.indran@ziffity.com>
2020-02-17 19:25:59 +0530

    header added

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import ReactDOM from "react-dom";
+import Header from './basics/Header'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edits <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="main" >
+      <Header />
     </div>
   );
 }
