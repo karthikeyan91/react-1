35011766ca816358ab25dbf403d2cbb0200b86d7
karthikeyan <karthikeyaninfo91@gmail.com>
2020-02-18 22:15:43 +0530

    footer

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 // import ReactDOM from "react-dom";
 import Header from './basics/Header'
-
+import Footer from './basics/Footer'
 function App() {
   return (
     <div className="main" >
       <Header />
+      <Footer />
     </div>
   );
 }
