8bc9a2cf12d7b88f97d5cd10ddc8d9b101a34eb3
karthikeyan <karthikeyaninfo91@gmail.com>
2020-02-19 23:05:52 +0530

    contact

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import {Route, NavLink, HashRouter} from 'react-router-dom'
 // import ReactDOM from "react-dom";
 import Header from './basics/Header';
 import Footer from './basics/Footer';
 import Home from './basics/Home';
+import Contact from "./basics/Contact";
 
 function App() {
   return (
--- hello/src/basics/Header.js
+++ hello/src/basics/Header.js
@@ -17,16 +17,8 @@ class Header extends React.Component {
             <li className="nav-item">
               <a className="nav-link" href="#">Products</a>
             </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#">Something else here</a>
-              </div>
+            <li className="nav-item">
+              <a className="nav-link" href="/contact">Contact</a>
             </li>
           </ul>
         </div>
