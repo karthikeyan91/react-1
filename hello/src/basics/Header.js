e5c13c923827b048b5002ed44f150a98343ea0f4
Karthikeyan <karthikeyan.indran@ziffity.com>
2020-02-20 11:43:34 +0530

    all

--- hello/src/basics/Header.js
+++ hello/src/basics/Header.js
@@ -3,24 +3,19 @@ import {NavLink} from 'react-router-dom'
 class Header extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light pl-3 pr-3 pt-0 pb-0">
 
         <NavLink className="navbar-brand" exact to="/">Ecomm</NavLink>
-
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <NavLink className="nav-link" exact to="/">Home</NavLink>
+              <NavLink className="nav-link p-3" exact to="/">Home</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/list">Products</NavLink>
+              <NavLink className="nav-link p-3" to="/list">Products</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/contact">Contact</NavLink>
+              <NavLink className="nav-link p-3" to="/contact">Contact</NavLink>
             </li>
           </ul>
         </div>
